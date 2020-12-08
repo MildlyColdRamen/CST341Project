0ded2c93989510bb533c5364f200de3ac5d1909c
Thor.Abe <Thor.Abe@192.168.0.33>
2020-12-07 23:43:09 -0700

    This is my portion of the project

--- src/edu/gcu/cst341/project/MyStore.java
+++ src/edu/gcu/cst341/project/MyStore.java
@@ -2,6 +2,8 @@ package edu.gcu.cst341.project;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MyStore {
 
@@ -104,41 +106,108 @@ public class MyStore {
 			open();
 		}
 	}
-	
+
 	private void createCartItem() {
 		System.out.println("Add (Create) item to cart...");
 		System.out.println();
 	}
-	
+
 	private void readCartItems() {
 		System.out.println("View (Read) cart...");
 		System.out.println();
 	}
-	
+
 	private void deleteCartItem() {
 		System.out.println("Delete from cart...");
 		System.out.println();
 	}
-	
+
 	private void createProduct() {
 		System.out.println("Create product...");
 		System.out.println();
 	}
-	
+
 	private void readProducts() {
 		System.out.println("View (Read) all products...");
 		System.out.println();
+		try(Statement stmt = con.getConnection().createStatement();){
+
+			try (ResultSet rs = stmt.executeQuery("SELECT productId, productName, productPrice, stockStatus FROM cst341n.products;")){
+				while(rs.next()) {
+					System.out.println("ID: |" + rs.getInt("productId") + "| " + rs.getString("productName") +
+							"| Price: " + rs.getBigDecimal("productPrice") + "| In Stock?: " + rs.getBoolean("stockStatus"));
+					System.out.println();
+				}
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 	}
-	
+
 	private void updateProduct() {
 		System.out.println("Update product...");
 		System.out.println();
 	}
-	
+
 	private void deleteProduct() {
 		System.out.println("Delete product...");
-		System.out.println();
+		System.out.println("Showing all products...");
+
+		try(Statement stmt = con.getConnection().createStatement();){
+
+			try (ResultSet rs = stmt.executeQuery("SELECT productId, productName, productPrice, stockStatus FROM cst341n.products;")){
+				while(rs.next()) {
+					System.out.println("ID: |" + rs.getInt("productId") + "| " + rs.getString("productName") +
+							"| Price: " + rs.getBigDecimal("productPrice") + "| In Stock?: " + rs.getBoolean("stockStatus"));
+				}
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		System.out.println("Which product would you like to delete? (Select ID Number");
+		int idNumber = UserInterface.sc.nextInt();
+		UserInterface.sc.nextLine();
+		String sql = "Delete From cst341n.products where productId = ?;";
+		try (PreparedStatement ps = con.getConnection().prepareStatement(sql)){
+			ps.setInt(1, idNumber);
+			ps.execute();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Deleted Select Product");
+		try(Statement stmt = con.getConnection().createStatement();){
+
+			try (ResultSet rs = stmt.executeQuery("SELECT productId, productName, productPrice, stockStatus FROM cst341n.products;")){
+//				Remember to changed it to the name of your database for the group project
+				while(rs.next()) {
+					System.out.println("ID: |" + rs.getInt("productId") + "| " + rs.getString("productName") +
+							"| Price: " + rs.getBigDecimal("productPrice") + "| In Stock?: " + rs.getBoolean("stockStatus"));
+					System.out.println();
+				}
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		
 	}
+	//	add method to print out name
+	//	Abraham 11/19/2020
+    // Added to alllow...
+	// Abraham Gamez - 12/07/2020
 	private void abraham() {
 		System.out.println("Abraham");
 	}
